90b599b9118bf456ebadcd92f45a01407b88e5b2
ksk-tngc <59589496+ksk-tngc@users.noreply.github.com>
2021-11-10 13:50:10 +0900

    feat: グローバルスタイルを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { Login } from './components/pages/Login'
+import { theme } from './theme/theme'
 
 export const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <ChakraProvider theme={theme}>
+      <div>
+        <Login />
+      </div>
+    </ChakraProvider>
   )
 }
